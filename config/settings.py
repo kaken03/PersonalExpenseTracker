51ba9d9f6a29d7b49d8e8159f4421fc738cc0a41
kaken03 <kentezekielvillahermosa@gmail.com>
2026-02-13 18:30:15 +0800

    Fix: Add Railway domain to ALLOWED_HOSTS in settings.py

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-xk-ph3vb-h%6hmtin&gi9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=Csv())
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1,personalexpensetracker.up.railway.app', cast=Csv())
 
 
 # Application definition
