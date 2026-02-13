39103bd8c575cbc989bf738a7e8e3eed5c3bd234
kaken03 <kentezekielvillahermosa@gmail.com>
2026-02-13 18:32:42 +0800

    Fix: Add CSRF_TRUSTED_ORIGINS for Railway domain

--- config/settings.py
+++ config/settings.py
@@ -28,6 +28,13 @@ DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1,personalexpensetracker.up.railway.app', cast=Csv())
 
+# CSRF Settings
+CSRF_TRUSTED_ORIGINS = [
+    'https://personalexpensetracker.up.railway.app',
+    'http://localhost:8000',
+    'http://127.0.0.1:8000',
+]
+
 
 # Application definition
 
